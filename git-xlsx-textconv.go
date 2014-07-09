81248098681a800bb983f0ce8aa6d6b535ba5a70
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2014-07-10 04:49:42 +0900

    remove garbage tabs

--- git-xlsx-textconv.go
+++ git-xlsx-textconv.go
@@ -23,13 +23,13 @@ func main() {
 	for _, sheet := range xlFile.Sheets {
 		for _, row := range sheet.Rows {
 			cels := make([]string, len(row.Cells))
-			for _, cell := range row.Cells {
+			for i, cell := range row.Cells {
 				s := cell.String()
 				s = strings.Replace(s, "\\", "\\\\", -1)
 				s = strings.Replace(s, "\n", "\\n", -1)
 				s = strings.Replace(s, "\r", "\\r", -1)
 				s = strings.Replace(s, "\t", "\\t", -1)
-				cels = append(cels, s)
+				cels[i] = s
 			}
 			fmt.Printf("[%s] %s\n", sheet.Name, strings.Join(cels, "\t"))
 		}
